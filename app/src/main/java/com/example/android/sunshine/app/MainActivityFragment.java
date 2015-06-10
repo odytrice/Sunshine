ddb089d46aa38db405661ca9a584235ae3064480
Ody Mbegbu <odytrice@gmail.com>
2015-06-10 22:40:53 +0100

    Initial Load of Sunshine App

---
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -0,0 +1,36 @@
+package com.example.android.sunshine.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+        String[] itemsArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 70/46",
+                "Sat - HELP TRAPPED IN WEATHER STATION - 76/68",
+                "Sunday - Sunny - 80/68"
+        };
+        ArrayList<String> items = new ArrayList<>(Arrays.asList(itemsArray));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
