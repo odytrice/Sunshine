986cf559357320c3b35b0734c27813684cf04b17
Ody Mbegbu <odytrice@gmail.com>
2015-06-12 06:22:16 +0100

    Completed List View Display

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,21 +18,30 @@ import java.util.Arrays;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         String[] itemsArray = {
                 "Today - Sunny - 88/63",
                 "Tomorrow - Foggy - 70/46",
                 "Weds - Cloudy - 72/63",
                 "Thurs - Rainy - 64/51",
                 "Fri - Foggy - 70/46",
-                "Sat - HELP TRAPPED IN WEATHER STATION - 76/68",
+                "Sat - Sunny - 76/68",
                 "Sunday - Sunny - 80/68"
         };
-        ArrayList<String> items = new ArrayList<>(Arrays.asList(itemsArray));
-    }
+        ArrayList<String> weekForecast = new ArrayList<>(Arrays.asList(itemsArray));
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView list = (ListView)rootView.findViewById(R.id.listview_forecast);
+        list.setAdapter(adapter);
+
+        return rootView;
     }
 }
